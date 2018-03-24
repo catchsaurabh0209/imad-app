78f84ecec650e71724eb2798e6b217f7cc20c676
catchsaurabh0209 <37589787+catchsaurabh0209@users.noreply.github.com>
2018-03-24 22:25:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/new-file',function(req,res){
     
 });
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__article-one, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
         });
 
 
